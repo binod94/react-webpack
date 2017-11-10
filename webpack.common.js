8da9dae897ace8814d4ee2fffb1df010c016018f
Binod Kumar Swain <binodkumarswain94@gmail.com>
2017-11-10 18:28:56 +0530

    added css and url loader

--- webpack.common.js
+++ webpack.common.js
@@ -1,36 +1,50 @@
- const path = require('path');
- const CleanWebpackPlugin = require('clean-webpack-plugin');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body',
-  title: 'React App'
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body',
+  title: 'React App'
 })
 
- module.exports = {
-   entry: {
-     app: './src/index.js'
-   },
-   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    HtmlWebpackPluginConfig
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist')
-   },
-   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  },
+module.exports = {
+   entry: {
+     app: './src/index.js'
+   },
+   plugins: [
+    new CleanWebpackPlugin(['dist']),
+    HtmlWebpackPluginConfig
+   ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist')
+   },
+   module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(sass|scss|css)$/, //Check for sass or scss file names
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader",
+        query: { mimetype: "image/png" }
+      }
+    ]
+  },
 
- };
+};
+
